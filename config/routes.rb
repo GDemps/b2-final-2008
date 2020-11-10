3f19a7b4f1e7e4746c81033330e4c833cf536ee3
Grant Dempsey <CryptoDemps@gmail.com>
2020-11-09 17:50:48 -0700

    Nested route to scope doctor with patient

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: [:show]
-
-  resources :doctor_patients, only: [:destroy]
+  resources :doctors, only: [:show] do
+    resources :doctor_patients, only: [:destroy]
+  end
 
   resources :hospitals, only: [:show]
 end
